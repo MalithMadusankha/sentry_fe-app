aa10be08039f209dd64d210570da21e6012e87db
Malith Madusankha <malithsony@gmail.com>
2022-10-10 11:24:02 +0530

    componet

---
+++ src/components/Footers/SimpleFooter.js
@@ -0,0 +1,63 @@
+
+import React from "react";
+// reactstrap components
+import {
+  Button,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip
+} from "reactstrap";
+
+class SimpleFooter extends React.Component {
+  render() {
+    return (
+      <>
+        <footer className=" footer">
+          <Container>
+            <hr />
+            <Row className=" align-items-center justify-content-md-between">
+              <Col md="6">
+                <div className=" copyright">
+                  © {new Date().getFullYear()}{" "}
+                  <a
+                    href="https://www.creative-tim.com?ref=adsr-footer"
+                    target=""
+                  >
+                    SERENITY
+                  </a>
+                  .
+                </div>
+              </Col>
+              <Col md="6">
+                <Nav className=" nav-footer justify-content-end">
+                  <NavItem>
+                    <NavLink
+                      href="https://www.creative-tim.com?ref=adsr-footer"
+                      target="_blank"
+                    >
+                      CONTACT US
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      href="https://www.creative-tim.com/presentation?ref=adsr-footer"
+                      target="_blank"
+                    >
+                      About Us
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </>
+    );
+  }
+}
+
+export default SimpleFooter;
---
+++ src/components/Navbars/DemoNavbar.js
@@ -0,0 +1,165 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// JavaScript plugin that hides or shows a component based on your scroll
+import Headroom from "headroom.js";
+// reactstrap components
+import {
+  UncontrolledCollapse,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+import Logo from '../../assets/img/logo.png'
+
+class DemoNavbar extends React.Component {
+  componentDidMount() {
+    let headroom = new Headroom(document.getElementById("navbar-main"));
+    // initialise
+    headroom.init();
+  }
+  state = {
+    collapseClasses: "",
+    collapseOpen: false,
+  };
+
+  onExiting = () => {
+    this.setState({
+      collapseClasses: "collapsing-out",
+    });
+  };
+
+  onExited = () => {
+    this.setState({
+      collapseClasses: "",
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <header className="header-global">
+          <Navbar
+            className="navbar-main navbar-transparent navbar-light headroom pt-0"
+            expand="lg"
+            id="navbar-main"
+          >
+            <Container>
+              <NavbarBrand className="mr-lg-5 pt-0" to="/" tag={Link}>
+                <img
+                  alt="..."
+                  src={Logo}
+                  style={{height: 100}}
+                />
+              </NavbarBrand>
+              <button className="navbar-toggler" id="navbar_global">
+                <span className="navbar-toggler-icon" />
+              </button>
+              <UncontrolledCollapse
+                toggler="#navbar_global"
+                navbar
+                className={this.state.collapseClasses}
+                onExiting={this.onExiting}
+                onExited={this.onExited}
+              >
+                <div className="navbar-collapse-header">
+                  <Row>
+                    <Col className="collapse-brand" xs="6">
+                      <Link to="/">
+                        <img
+                          alt="..."
+                          src={require("assets/img/brand/argon-react.png")}
+                        />
+                      </Link>
+                    </Col>
+                    <Col className="collapse-close" xs="6">
+                      <button className="navbar-toggler" id="navbar_global">
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+                <Nav className="navbar-nav-hover align-items-lg-center" navbar>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.facebook.com/creativetim"
+                      id="tooltip333589074"
+                      target="_blank"
+                    >
+                      
+                      <span className="p-2">Home</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.facebook.com/creativetim"
+                      id="tooltip333589074"
+                      target="_blank"
+                    >
+                      
+                      <span className="p-2">CONTACT US</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.facebook.com/creativetim"
+                      id="tooltip333589074"
+                      target="_blank"
+                    >
+                      
+                      <span className="p-2">ABOUT US</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.facebook.com/creativetim"
+                      id="tooltip333589074"
+                      target="_blank"
+                    >
+                      
+                      <span className="p-2">PTIENTS PROFILE</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.facebook.com/creativetim"
+                      id="tooltip333589074"
+                      target="_blank"
+                    >
+                      
+                      <span className="p-2">DOCTOR PROFILE</span>
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.facebook.com/creativetim"
+                      id="tooltip333589074"
+                      target="_blank"
+                    >
+                      
+                      <span className="p-2">SIGN OUT</span>
+                    </NavLink>
+                  </NavItem>
+                </Nav>
+              </UncontrolledCollapse>
+            </Container>
+          </Navbar>
+        </header>
+      </>
+    );
+  }
+}
+
+export default DemoNavbar;
